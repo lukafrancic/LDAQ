65682929ec14a07820485512b0b445752cc11c73
Luka Francic <79054703+lukafrancic@users.noreply.github.com>
2024-09-09 21:26:06 +0200

    dodan Scanner class za nastavljanje laserja, zajem locen v svojem threadu

--- LDAQ/micro_epsilon/acquisition.py
+++ LDAQ/micro_epsilon/acquisition.py
@@ -2,6 +2,7 @@ import numpy as np
 
 import ctypes as ct
 import threading
+import time
 
 from ..acquisition_base import BaseAcquisition
 
@@ -123,18 +124,14 @@ class Scanner:
         self.exposure_time = exposure_time
         self.idle_time = idle_time
         self.resolution_id = resolution_id
-        #TODO to bi bilo fajn, ce se nastavi v MELLT classu
-        if channel_names is None:
-            self._channel_names_init = list(self._default_channel_names.keys())
-        else:
-            self._channel_names_init = channel_names
 
-        self.channel_idx = [i for i in range(len(self._channel_names_init))]
         self.hLLT = llt.create_llt_device(llt.TInterfaceType.INTF_TYPE_ETHERNET)
         self._initialize_scanner()
-        self.is_on = False
+        self.is_on = True
         self._connect_scanner()
         self._setup_laser()
+        self.update_event = None
+        self._termination_flag = False
 
 
     def _initialize_scanner(self):
@@ -169,8 +166,6 @@ class Scanner:
 
         if ret < 1:
             raise Exception(f"Error getting scanner type: {ret} ")
-        
-        self.turn_on_laser()
 
 
     def disconnect_scanner(self):
@@ -273,9 +268,7 @@ class Scanner:
         # korektno izracunaj sample_rate
         # dobimo v hz
         self.sample_rate = 100_000/(self.exposure_time+self.idle_time)
-        print(f"f = {self.sample_rate:.1f} hz")
-
-        # self.profile_setup()
+        # print(f"f = {self.sample_rate:.1f} hz")
 
 
     def profile_setup(self, channel_names):
@@ -305,13 +298,13 @@ class Scanner:
         if ret < 1:
             raise Exception(f"Error setting profile config: {ret}" )
         
-        self.partial_profile_struct = llt.TPartialProfile(
+        self._partial_profile_struct = llt.TPartialProfile(
             0, self.start_data, self.resolution, self.data_width
             )
 
         # set partial profile
         ret = llt.set_partial_profile(
-            self.hLLT, ct.byref(self.partial_profile_struct)
+            self.hLLT, ct.byref(self._partial_profile_struct)
             )
         if ret < 1:
             raise Exception(f"Error setting partial profile: {ret}")
@@ -377,6 +370,10 @@ class MELaserScanner(BaseAcquisition):
         
         Returns None.
         """
+        self.device._termination_flag = True
+
+        self.acq_thread.join()
+
         self.device._stop_transfer()
 
 
@@ -393,364 +390,92 @@ class MELaserScanner(BaseAcquisition):
 
     def set_data_source(self) -> None:
         """
-        TODO kar zacnemo s prenosom? je to dejansko ok, ker v .core tega ni
+        setup the data source and start the profile transfer.
         """
 
-        self.buffer = Buffer(self.resolution, self.data_width)
-        get_profile_cb = llt.buffer_cb_func(self.buffer)
-
-        ret = llt.register_callback(
-            self.hLLT, llt.TCallbackType.C_DECL, get_profile_cb, 1
-            )
-        get_profile_cb = llt.buffer_cb_func(self.buffer)
-
-        ret = llt.register_callback(
-            self.hLLT, llt.TCallbackType.C_DECL, get_profile_cb, 1
-            )
-        if ret < 1:
-            raise Exception(f"Error setting callback: {ret}")
-
-        ret = llt.transfer_profiles(self.hLLT, llt.TTransferProfileType.NORMAL_TRANSFER, 1)
-        if ret < 1:
-            raise Exception(f"Error starting transfer profiles: {ret}")
-
-        self.generator_function = _data_generator(
-            self.hLLT, self.partial_profile_struct, self.scanner_type,
-            self._pointers, self.buffer
+        self.acq_thread = threading.Thread(
+            target = _read_data_loop, args = [self.device]
         )
+        self.acq_thread.start()
 
         super().set_data_source()
-    
-
-    def read_data(self) -> np.ndarray:
-        
-        # run the next iteration step
-        next(self.generator_function)
-
-        return np.array(
-                [self._buffer_arrays[name] for name in self._channel_names_init]
-            ).T
-
-
-
-class MELaserScanner2:
-    _default_channel_names = {
-        "X": [float, ct.c_double],
-        "Z": [float, ct.c_double],
-        "I": [int, ct.c_ushort],
-        "T": [int, ct.c_ushort],
-        "W": [int, ct.c_ushort],
-        "M0": [int, ct.c_uint],
-        "M1": [int, ct.c_uint]
-    }
-
-    def __init__(self, acquisition_name: str = None, exposure_time: int = 100,
-                 idle_time: int = 700, resolution_id: int = 0, 
-                 channel_names: list = None) -> None:
-        """
-        Args:
-            acquisition_name (str, optional): Name of the class. Dafaults to 
-                None.
-            exposure_time (int): Shutter open time, defaults to 100
-            idle_time (int): Shutter close time, defaults to 700
-            resolution (int): The scanner has multiple available resolutions, 
-                eg. ltt25xx has a list of posible values [640, 320, 160, 0]. 
-                Resolution must be an int 0-3. Defaut value is 0.
-            channel_names list[str]: a list of channel names, the available 
-                names are as following ["X", "Z", "I", "T", "W", "M0", "M1"],
-                where each channel represents a datatype transmited from the
-                scanner.
-        """
-        try:
-            llt
-        except:
-            raise Exception("Pyllt library not found. Please install it" \
-                            "before using this class.")
-        
-        if acquisition_name is None:
-            self.acquisition_name = "Micro Epsilon"
-        else:
-            self.acquisition_name = acquisition_name
-        
-        self.exposure_time = exposure_time
-        self.idle_time = idle_time
-        self.resolution_id = resolution_id
-        if channel_names is None:
-            self._channel_names_init = list(self._default_channel_names.keys())
-        else:
-            self._channel_names_init = channel_names
-
-        self.channel_idx = [i for i in range(len(self._channel_names_init))]
-        self.hLLT = llt.create_llt_device(llt.TInterfaceType.INTF_TYPE_ETHERNET)
-        self._initialize_scanner()
-        self.is_on = False
-        self._connect_scanner()
-        self._setup_laser()
-
-        self.set_data_source()
-
-
-    def _initialize_scanner(self):
-        """
-        seting up interface
-        """
-        available_interfaces = (ct.c_uint*6)()
-
-        ret = llt.get_device_interfaces_fast(self.hLLT, available_interfaces, 
-                                             len(available_interfaces))
-        if ret < 1:
-            raise ValueError("Error getting interfaces : " + str(ret))
-
-        ret = llt.set_device_interface(self.hLLT, available_interfaces[0], 0)
-        if ret < 1:
-            raise ValueError("Error setting device interface: " + str(ret))
-    
-
-    def _connect_scanner(self):
-        """
-        Connect to the scanner
-        """
-        ret = llt.connect(self.hLLT)
-
-        if ret < 1: 
-            raise Exception(f"Failed to connect to scanner:  {ret}")
-        
-        # get scanner type
-        self.scanner_type = ct.c_int(0)
-
-        ret = llt.get_llt_type(self.hLLT, ct.byref(self.scanner_type))
-
-        if ret < 1:
-            raise Exception(f"Error getting scanner type: {ret} ")
-        
-        self.turn_on_laser()
-
-
-    def disconnect_scanner(self):
-        """
-        Disconnect the scanner
-        """
-        self.turn_off_laser()
-
-        #TODO check if already disconnected?
-        ret = llt.disconnect(self.hLLT)
-        if ret < 1:
-            raise Exception(f"Error while disconnect: {ret}")
-
-
-    def turn_on_laser(self):
-        """
-        Method to turn on the laser.
-        """
-        if not self.is_on:
-            ret = llt.set_feature(self.hLLT, llt.FEATURE_FUNCTION_LASER, 2)
-
-            if ret < 1:
-                raise Exception(f"Failed to turn on the laser {ret}")
-
-            self.is_on = True
-        
-        else:
-            print("Laser is already turned on.")
-
-
-    def turn_off_laser(self):
-        """
-        Method to turn off the laser.
-        """
-        if self.is_on:
-            ret = llt.set_feature(self.hLLT, llt.FEATURE_FUNCTION_LASER, 0)
-
-            if ret < 1:
-                raise Exception(f"Failed to turn off the laser {ret}")
-            
-            self.is_on = False
-
-        else:
-            print("Laser is already turned off")
-
-    
-    def _setup_laser(self):
-        """
-        setup the scanner parameters
-
-        The sample rate is defined by exposure and idle time by the 
-        following equation:
-
-        f = 10^5/(t_Exposure + t_Idle) Hz
-
-        Times can be set with steps of 10 us (us -> micro seconds). So to
-        achieve an exposure time of 1 ms, exposure_time must be set as 100,
-        so -> 100 * 10 us = 1000 us.
-        """
-        available_resolutions = (ct.c_uint*4)()
-
-        # set resolution
-        if isinstance(self.resolution_id, int) and (self.resolution_id >= 0 and self.resolution_id <= 3):    
-            ret = llt.get_resolutions(
-                self.hLLT, available_resolutions, 4
-                )
-            if ret < 1:
-                raise ValueError(f"Error getting resolutions: {ret}")
-        else:
-            raise Exception(f"The passed value {self.resolution_id} for self.resolution is not valid." \
-                            "\nself.resolution must be an int 0-3!")
-
-        self.resolution = available_resolutions[self.resolution_id]
-
-        ret = llt.set_resolution(self.hLLT, self.resolution)
-        if ret < 1:
-            raise ValueError(f"Error setting resolution: {ret}")
-
-        # set sample rate
-        if isinstance(self.exposure_time, int) and isinstance(self.idle_time, int):
-            ret = llt.set_feature(
-                self.hLLT, llt.FEATURE_FUNCTION_EXPOSURE_TIME, self.exposure_time
-                )
-            if ret < 1:
-                raise ValueError(f"Error setting resolution: {ret}")
-            
-            ret = llt.set_feature(self.hLLT, llt.FEATURE_FUNCTION_IDLE_TIME, self.idle_time)
-            if ret < 1:
-                raise ValueError(f"Error setting resolution: {ret}")
-        else:
-            raise Exception(f"Passed values for exposure and idle time are not ints: " \
-                            f"({self.exposure_time}, {self.idle_time})")
-
-        #TODO to ne velja v primer laserjeve serije 30xx -> preverjaj verzijo in 
-        # korektno izracunaj sample_rate
-        self.sample_rate = 100_000/(self.exposure_time+self.idle_time) # dobimo v hz
-        print(f"f = {self.sample_rate:.1f} hz")
-
-        self.profile_setup()
-
-
-    def terminate_data_source(self):
-        """        
-        Properly closes acquisition source after the measurement.
-        
-        Returns None.
-        """
-        ret = llt.transfer_profiles(self.hLLT, llt.TTransferProfileType.NORMAL_TRANSFER, 0)
-        if ret < 1:
-            raise Exception(f"Error stopping transfer profiles: {ret}")
-
-
-    def get_sample_rate(self):
-        """
-        Returns sample rate of acquisition class.
-        This function is also useful to compute sample_rate estimation if no sample rate is given
-        
-        Returns self.sample_rate
-        """
-        return self.sample_rate
-    
-
-    def profile_setup(self):
-        #TODO ustrezno nastavi glede na zahtevane channele
-        self.start_data = 0
-        self.data_width = 16
-        # _profile_buffer = (ct.c_ubyte*(self.resolution * self.data_width))()
-        # c55
-        # self.scanner_buffer = Buffer(self.resolution, self.data_width)
-        self._pointers = {}
-        self._buffer_arrays = {}
-
-        for name, data_type in self._default_channel_names.items():
-            if name in self._channel_names_init:
-                self._buffer_arrays[name] = np.empty(self.resolution, dtype=data_type[0])
-                self._pointers[name] = self._buffer_arrays[name].ctypes.data_as(
-                    ct.POINTER(data_type[1])
-                )
-            else:
-                self._pointers[name] = ct.POINTER(data_type[1])()
-
-        # Set partial profile as profile config
-        ret = llt.set_profile_config(
-            self.hLLT, llt.TProfileConfig.PARTIAL_PROFILE
-            )
-        if ret < 1:
-            raise Exception(f"Error setting profile config: {ret}" )
-        
-        self.partial_profile_struct = llt.TPartialProfile(
-            0, self.start_data, self.resolution, self.data_width
-            )
-
-        # set partial profile
-        ret = llt.set_partial_profile(
-            self.hLLT, ct.byref(self.partial_profile_struct)
-            )
-        if ret < 1:
-            raise Exception(f"Error setting partial profile: {ret}")
-        
-        # c55
-        # get_profile_cb = llt.buffer_cb_func(self.scanner_buffer)
-        # get_profile_cb = llt.buffer_cb_func(_profile_callback)
-        # ret = llt.register_callback(
-        #     self.hLLT, llt.TCallbackType.C_DECL, get_profile_cb, 1
-        #     )
-        # if ret < 1:
-        #     raise Exception(f"Error setting callback: {ret}")
-
-
-    def set_data_source(self) -> None:
-        """
-        TODO kar zacnemo s prenosom? je to dejansko ok, ker v .core tega ni
-        """
-        # self.generator_function = self.data_generator()
-        # global _profile_buffer
-        # global _event
-
-        # _event = threading.Event()
-        # _profile_buffer = (ct.c_ubyte*(self.resolution * self.data_width))()
-        
-        self.buffer = Buffer(self.resolution, self.data_width)
-        get_profile_cb = llt.buffer_cb_func(self.buffer)
-        ret = llt.register_callback(
-            self.hLLT, llt.TCallbackType.C_DECL, get_profile_cb, 1
-            )
-        if ret < 1:
-            raise Exception(f"Error setting callback: {ret}")
-        
-        ret = llt.transfer_profiles(self.hLLT, llt.TTransferProfileType.NORMAL_TRANSFER, 1)
-        if ret < 1:
-            raise Exception(f"Error starting transfer profiles: {ret}")
-
-        self.generator_function = _data_generator(
-            self.hLLT, self.partial_profile_struct, self.scanner_type,
-            self._pointers, self.buffer
-        )
 
 
     def read_data(self) -> np.ndarray:
-        
-        # run the next iteration step
-        next(self.generator_function)
-
-        return np.array(
-                [self._buffer_arrays[name] for name in self._channel_names_init]
+        """
+        Read data from the scanner.
+        """
+        while True:
+            self.device.update_event.wait()
+            data = np.array(
+                [self.device._buffer_arrays[name] for name in self._channel_names_init]
             ).T
+            break
+
+        self.device.update_event.clear()
+
+        return data
 
 
-def _data_generator(hLLT, profile_struct, sc_type, pointer_dict, buffer):
-    # global _profile_buffer
-    # global _event
+def _data_generator(device: Scanner, buffer: Buffer):
+    """
+    Generator for data acquisition
+
+    device: instance of the Scanner class
+    buffer: instance of the Buffer class
+    """
 
     while True:
-        # _event.wait()
         buffer.event.wait()
 
         fret = llt.convert_part_profile_2_values(
-            hLLT, buffer.profile_buffer,
-            ct.byref(profile_struct), sc_type, 0,
-            1, pointer_dict["W"], pointer_dict["I"], pointer_dict["T"], 
-            pointer_dict["X"], pointer_dict["Z"], pointer_dict["M0"],
-            pointer_dict["M1"]
+            device.hLLT, buffer.profile_buffer,
+            ct.byref(device._partial_profile_struct), device.scanner_type, 0,
+            1, device._pointers["W"], device._pointers["I"], 
+            device._pointers["T"], device._pointers["X"], 
+            device._pointers["Z"], device._pointers["M0"],
+            device._pointers["M1"]
             )
-        
+
         if fret & llt.CONVERT_X == 0 or fret & llt.CONVERT_Z == 0:
-            raise Exception(f"Error converting data: {fret}")
-        
+            raise ValueError("Error converting data: " + str(fret))
+
         buffer.event.clear()
 
         yield
+
+
+def _read_data_loop(device: Scanner):
+    """
+    Function to run the acquisition generator in a loop
+    """
+    buffer = Buffer(device.resolution, device.data_width)
+    get_profile_cb = llt.buffer_cb_func(buffer)
+
+    ret = llt.register_callback(
+        device.hLLT, llt.TCallbackType.C_DECL, get_profile_cb, 1
+        )
+    get_profile_cb = llt.buffer_cb_func(buffer)
+
+    ret = llt.register_callback(
+        device.hLLT, llt.TCallbackType.C_DECL, get_profile_cb, 1
+        )
+    if ret < 1:
+        raise Exception(f"Error setting callback: {ret}")
+    ret = llt.transfer_profiles(device.hLLT, 
+                                llt.TTransferProfileType.NORMAL_TRANSFER, 1)
+    if ret < 1:
+        raise Exception(f"Error starting transfer profiles: {ret}")
+
+    time.sleep(0.1)
+
+    device.update_event = threading.Event()
+    gen_fun = _data_generator(device, buffer)
+
+    while True:
+        if device._termination_flag:
+            gen_fun.close()
+            break
+
+        next(gen_fun)
+        device.update_event.set()
