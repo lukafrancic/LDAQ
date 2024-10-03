43bc9249dc857f045b4df63a8e908d06d00cf100
Luka Francic <79054703+lukafrancic@users.noreply.github.com>
2024-10-03 21:19:47 +0200

    Scanner class is now a standalone.

--- LDAQ/micro_epsilon/acquisition.py
+++ LDAQ/micro_epsilon/acquisition.py
@@ -12,10 +12,32 @@ except:
     pass
 
 
-class Buffer:
-    def __init__(self, resolution, data_width):
+# datatypes for setting up proper buffer arrays
+# using appropriate data types to reduce memory storage
+# first list value to set numpy type, second for the C array for the scanner
+_default_channel_names = {
+        "X": [np.float64, ct.c_double],
+        "Z": [np.float64, ct.c_double],
+        "I": [np.int16, ct.c_ushort],
+        "T": [np.int16, ct.c_ushort],
+        "W": [np.int16, ct.c_ushort],
+        "M0": [np.int32, ct.c_uint],
+        "M1": [np.int32, ct.c_uint]
+    }
+
+
+
+class ProfileBuffer:
+    """
+    Temporary profile buffer with a custom callback.
+    """
+    def __init__(self, resolution: int, data_width: int):
         """
         Class for storing the temporary profile buffer, with callback function.
+        
+        args:
+            resolutin: int
+            data_width: int
         """
         self.profile_buffer = (ct.c_ubyte*(resolution * data_width))()
         self.event = threading.Event()
@@ -27,6 +49,111 @@ class Buffer:
             ct.memmove(self.profile_buffer, data, size)
 
             self.event.set()
+
+
+
+class Flag:
+    """
+    Class for storing the flag value
+    """
+    def __init__(self, val = True):
+        self.value = val
+
+
+
+class Buffer:
+    """
+    Class that encapsulates all the transmited data from the scanner.
+
+    Available methods:
+    self.clear_buffer()
+    self.add_to_buffer()
+
+    Available atributes:
+    self.data -> dict that holds all the data
+    """
+    def __init__(self, channel_names: dict, resolution: int, 
+                 sample_rate: float):
+        """
+        Class for storing the data from the scanner.
+
+        Args:
+            channel_names: used channel names
+            resolution: the used resolution
+            sample_rate: the used sample rate, used to estimate the buffer size
+            profile_buffer: dict that contains the current profile data with
+                numpy arrays as items and channel names as keys
+        """
+        self._channel_names = channel_names
+        self._resolution = resolution
+        # counter to track the number of transmited profiles, it is used to
+        # update the buffer size
+        self._profile_count = 0
+        self.data = {}
+        # dict za shranjevanje pointerjev do C buffer array-ev
+        self._pointers = {}
+        # dict, kjer se shranijo prazni array-i, v teh array-ih se shranujejo
+        # novo zajeti profili
+        self._profile_buffer = {}
+        # define the initial buffer size for a 1s long measurement
+        self._buffer_size = int(1/sample_rate)+1
+
+
+    def clear_buffer(self):
+        """
+        Clears the buffer. It is also used to setup the buffer.
+
+        Caution! This will delete all the measurement data.
+        """
+        for name, data_type in _default_channel_names.items():
+            if name in self._channel_names:
+                self._profile_buffer[name] = np.empty(self._resolution, 
+                                                     dtype=data_type[0])
+                self._pointers[name] = self._profile_buffer[name].ctypes.data_as(
+                    ct.POINTER(data_type[1])
+                )
+                # buffer za 1s zajema
+                self.data[name] = np.zeros(
+                    (self._resolution, self._buffer_size), dtype=data_type[0])
+                
+            else:
+                self._pointers[name] = ct.POINTER(data_type[1])()
+
+        self._current_size = self._buffer_size
+
+
+    def _extend_buffer(self) -> None:
+        """
+        Increase the buffer size for another 1s of measurement.
+        """
+        #TODO ne vem, ce bo to dejansko dovolj hitro...
+        for name in self._channel_names:
+            self.data[name] = np.vstack((
+                self.data[name], np.zeros(
+                    (self._resolution, self._buffer_size),
+                    dtype = _default_channel_names[name][0])
+            ))
+
+        self._current_size += self._buffer_size
+
+
+    def add_to_buffer(self) -> None:
+        """
+        Append new data to the buffer.
+        """
+        #TODO to naceloma dela samo za X, Z, M0, M1 podatke, za I, W, T je
+        # bila tezava pri pretvarjanju na RPI-ju, glej resitev v "scan" 
+        # funkciji -> resitev je, da gres na C array preko pointerja in
+        # iteriras cez njega ter zapisujes podatke
+        for name in self._channel_names:
+            self.data[name][
+                self._profile_count] += self._profile_buffer[name]
+
+        self._profile_count += 1
+
+        # damo si 10 profilov manj, ce se extendanje izvaja predolgo
+        if self._profile_count > self._current_size - 10:
+            self._extend_buffer()
 
 
 
@@ -88,31 +215,20 @@ class Scanner:
     Pyllt should now be installed inside your venv and can be imported like a
     normal package.
     """
-    _default_channel_names = {
-        "X": [float, ct.c_double],
-        "Z": [float, ct.c_double],
-        "I": [int, ct.c_ushort],
-        "T": [int, ct.c_ushort],
-        "W": [int, ct.c_ushort],
-        "M0": [int, ct.c_uint],
-        "M1": [int, ct.c_uint]
-    }
 
-    def __init__(self, exposure_time: int = 100,
+    def __init__(self, channel_names: dict = None, exposure_time: int = 100,
                  idle_time: int = 700, resolution_id: int = 0):
         """
         Args:
-            acquisition_name (str, optional): Name of the class. Dafaults to 
-                None.
-            exposure_time (int): Shutter open time, defaults to 100
-            idle_time (int): Shutter close time, defaults to 700
-            resolution (int): The scanner has multiple available resolutions, 
-                eg. ltt25xx has a list of posible values [640, 320, 160, 0]. 
-                Resolution must be an int 0-3. Defaut value is 0.
             channel_names list[str]: a list of channel names, the available 
                 names are as following ["X", "Z", "I", "T", "W", "M0", "M1"],
                 where each channel represents a datatype transmited from the
-                scanner.
+                scanner. If None all channels are used.
+            exposure_time (int): Shutter open time, defaults to 100
+            idle_time (int): Shutter close time, defaults to 700
+            resolution_id (int): The scanner has multiple available resolutions, 
+                eg. ltt25xx has a list of posible values [640, 320, 160, 0]. 
+                Resolution must be an int 0-3. Defaut value is 0.
         """
 
         try:
@@ -124,21 +240,35 @@ class Scanner:
         self.exposure_time = exposure_time
         self.idle_time = idle_time
         self.resolution_id = resolution_id
-
+        # check the key values
+        if channel_names is None:
+            self.channel_names = list(_default_channel_names.keys())
+        else:
+            for key in channel_names.keys():
+                if key not in _default_channel_names.keys():
+                    raise ValueError(f"Invalid channel name {key}")
+            self.channel_names = channel_names
+        # referenca za nastavljanje skenerja -> nisem testiral ce dejansko dela
+        self.llt = llt
+        # hLLT je v sami C implementaciji struct s podatki o skenerju, tukaj pa
+        # je dejansko zgolj int, posledicno bi lahko povsod samo pisal 1
         self.hLLT = llt.create_llt_device(llt.TInterfaceType.INTF_TYPE_ETHERNET)
         self._initialize_scanner()
-        self.is_on = True
         self._connect_scanner()
         self._setup_laser()
-        self.update_event = threading.Event()
-        self._termination_flag = False
-        self.llt = llt
-        self.data_buffer = []
+        # flag ce je vklopljen laser, naceloma je vklopljen pri priklopitvi na
+        # elektriko
+        self.is_on = True
+        # when set to True, it stops the acq. custom thread
+        # using a simple class so it gets passed by reference
+        # when started the value gets set to False, must be True to start the
+        # measurement
+        self._termination_flag = Flag(value = True)
 
 
     def _initialize_scanner(self):
         """
-        seting up interface
+        seting up interface for later connection.
         """
         available_interfaces = (ct.c_uint*6)()
 
@@ -154,7 +284,7 @@ class Scanner:
 
     def _connect_scanner(self):
         """
-        Connect to the scanner
+        Connects the PC to the scanner.
         """
         ret = llt.connect(self.hLLT)
 
@@ -273,27 +403,12 @@ class Scanner:
         # print(f"f = {self.sample_rate:.1f} hz")
 
 
-    def profile_setup(self, channel_names):
+    def profile_setup(self):
         #TODO ustrezno nastavi glede na zahtevane channele
+        # pri teh nastavitvah vedno prenasamo vse podatke -> lahko se optimira
+        # kolicina prenesenih podatkov
         self.start_data = 0
         self.data_width = 16
-        # _profile_buffer = (ct.c_ubyte*(self.resolution * self.data_width))()
-        # c55
-        # self.scanner_buffer = Buffer(self.resolution, self.data_width)
-        self._pointers = {}
-        self._buffer_arrays = {}
-        # moramo si shranit, ker rabimo v _read_data_loop-u
-        self._channel_names = channel_names
-
-        for name, data_type in self._default_channel_names.items():
-            if name in channel_names:
-                self._buffer_arrays[name] = np.empty(self.resolution, 
-                                                     dtype=data_type[0])
-                self._pointers[name] = self._buffer_arrays[name].ctypes.data_as(
-                    ct.POINTER(data_type[1])
-                )
-            else:
-                self._pointers[name] = ct.POINTER(data_type[1])()
 
         # Set partial profile as profile config
         ret = llt.set_profile_config(
@@ -312,18 +427,57 @@ class Scanner:
             )
         if ret < 1:
             raise Exception(f"Error setting partial profile: {ret}")
-        
 
-    def _stop_transfer(self) -> None:
+
+    def get_data(self):
         """
-        Stop profile transfer
+        Getter method to get the measurement
+        """
+        if self._termination_flag.value:
+            return self._data_buffer.data
         
-        Returns None.
+        else:
+            print("Measurement has not ended yet.")
+
+
+    def start_measurement(self):
         """
-        ret = llt.transfer_profiles(self.hLLT, 
-                            llt.TTransferProfileType.NORMAL_TRANSFER, 0)
-        if ret < 1:
-            raise Exception(f"Error stopping transfer profiles: {ret}")
+        Method to start the measurement.
+
+        Important note, when a new measurement gets started a new data buffer
+        is created.
+        """
+
+        if self._termination_flag.value:
+            # all data gets stored in the custom buffer class
+            self._data_buffer = Buffer(self.channel_names, self.resolution, 
+                                    self.sample_rate)
+            
+            self._termination_flag.value = False
+            self._data_buffer.clear_buffer()
+
+            # ni najbolj lepo...
+            self.acq_thread = threading.Thread(
+                target = _read_data_loop, args = [
+                    self.resolution, self.data_width,
+                    self.hLLT, self._termination_flag,
+                    self._partial_profile_struct, self.scanner_type,
+                    self._data_buffer, self.update_event
+                ]
+            )
+            self.acq_thread.start()
+
+        else:
+            print("A measurement seems to be running already.")
+
+
+    def stop_measurement(self):
+        """
+        Method to stop the measurement.
+        """
+        self._termination_flag.value = True
+
+        self.acq_thread.join()
 
 
 
@@ -359,15 +513,15 @@ class MELaserScanner(BaseAcquisition):
             #TODO check for correct channel names?
             self._channel_names = channel_names
 
-        self._channel_names_init = [
-                f"{name}_{i}" for name in self._channel_names for i in range(
-                self.device.resolution)
-            ]
+        # self._channel_names_init = [
+        #         f"{name}_{i}" for name in self._channel_names for i in range(
+        #         self.device.resolution)
+        #     ]
 
         # self._channel_names_init = ["all"]
         # self._spacer = self.device.resolution * len(self._channel_names)
-        # self._channel_names_video_init = self._channel_names
-        # self._channel_shapes_video_init = [[640,1] for i in range(len(self._channel_names))]
+        self._channel_names_video_init = self._channel_names
+        self._channel_shapes_video_init = [[640,1] for i in range(len(self._channel_names))]
 
         self.channel_idx = [i for i in range(len(self._channel_names))]
 
@@ -422,25 +576,26 @@ class MELaserScanner(BaseAcquisition):
         """
         Read data from the scanner.
         """
-        while True:
             
-            # vrne False, ce se izvede timeout
-            _flag = self.device.update_event.wait(self._timeout_time)
+        # vrne False, ce se izvede timeout
+        _flag = self.device.update_event.wait(self._timeout_time)
 
-            data = np.array(
-                self.device.data_buffer
-                ).T
-            print(data.shape)
-            if len(data.shape) < 1:
-                data.reshape(-1, 1)
-                print("Only 1")
-            if not _flag:
-                data = np.zeros(self.device.resolution*len(self._channel_names)
-                                ).reshape(-1,1)
-                print("Added an empty profile, proceed with caution")
-            
-            self.clear_buffer()
-            break
+        #TODO nov problem LDAQ-a -> podatki so zlozeni v dict, kjer ima posamezen
+        # array svoj tip podatka
+        data = np.array(
+            self.device.data_buffer
+            ).T
+        print(data.shape)
+        if len(data.shape) < 1:
+            data.reshape(-1, 1)
+            print("Only 1")
+        if not _flag:
+            data = np.zeros(self.device.resolution*len(self._channel_names)
+                            ).reshape(-1,1)
+            print("Added an empty profile, proceed with caution")
+        
+        self.clear_buffer()
+        
 
         self.device.update_event.clear()
 
@@ -452,28 +607,30 @@ class MELaserScanner(BaseAcquisition):
         clear the device buffer list
         """
 
-        self.device.data_buffer = []
+        self.device.clear_buffer()
 
 
 
-def _data_generator(device: Scanner, buffer: Buffer):
+def _data_generator(buffer: ProfileBuffer, hLLT: int,
+                    partial_profile_struct, scanner_type: int,
+                    pointers: dict):
     """
     Generator for data acquisition
 
     device: instance of the Scanner class
-    buffer: instance of the Buffer class
+    buffer: instance of the ProfileBuffer class
     """
 
     while True:
         buffer.event.wait()
 
         fret = llt.convert_part_profile_2_values(
-            device.hLLT, buffer.profile_buffer,
-            ct.byref(device._partial_profile_struct), device.scanner_type, 0,
-            1, device._pointers["W"], device._pointers["I"], 
-            device._pointers["T"], device._pointers["X"], 
-            device._pointers["Z"], device._pointers["M0"],
-            device._pointers["M1"]
+            hLLT, buffer.profile_buffer,
+            ct.byref(partial_profile_struct), scanner_type, 0,
+            1, pointers["W"], pointers["I"], 
+            pointers["T"], pointers["X"], 
+            pointers["Z"], pointers["M0"],
+            pointers["M1"]
             )
 
         if fret & llt.CONVERT_X == 0 or fret & llt.CONVERT_Z == 0:
@@ -481,29 +638,30 @@ def _data_generator(device: Scanner, buffer: Buffer):
 
         buffer.event.clear()
 
+        #TODO smiselno bi bilo se shraniti profile count oz. cas zajema profila
+        # in iz tega razbrati ali dejansko zajemamo vse profile
+
         yield
 
 
-def _read_data_loop(device: Scanner):
+def _read_data_loop(resolution: int, data_width: int, hLLT: int,
+                    termination_flag: Flag, partial_profile_struct,
+                    scanner_type: int, buffer: Buffer):
     """
     Function to run the acquisition generator in a loop
     """
-    buffer = Buffer(device.resolution, device.data_width)
-    get_profile_cb = llt.buffer_cb_func(buffer)
+    profile_buffer = ProfileBuffer(resolution, data_width)
+    get_profile_cb = llt.buffer_cb_func(profile_buffer)
 
     ret = llt.register_callback(
-        device.hLLT, llt.TCallbackType.C_DECL, get_profile_cb, 1
+        hLLT, llt.TCallbackType.C_DECL, get_profile_cb, 1
         )
-    get_profile_cb = llt.buffer_cb_func(buffer)
 
-    ret = llt.register_callback(
-        device.hLLT, llt.TCallbackType.C_DECL, get_profile_cb, 1
-        )
     if ret < 1:
         raise Exception(f"Error setting callback: {ret}")
     
     time.sleep(0.1)
-    ret = llt.transfer_profiles(device.hLLT, 
+    ret = llt.transfer_profiles(hLLT, 
                                 llt.TTransferProfileType.NORMAL_TRANSFER, 1)
     if ret < 1:
         raise Exception(f"Error starting transfer profiles: {ret}")
@@ -511,25 +669,21 @@ def _read_data_loop(device: Scanner):
     time.sleep(0.1)
 
     # device.update_event = threading.Event()
-    gen_fun = _data_generator(device, buffer)
+    gen_fun = _data_generator(buffer, hLLT, partial_profile_struct,
+                              scanner_type, buffer._pointers)
 
-    i = 0
     while True:
-        if device._termination_flag:
-            device._stop_transfer()
+        if termination_flag.value:
+            # device._stop_transfer()
+            ret = llt.transfer_profiles(hLLT, 
+                    llt.TTransferProfileType.NORMAL_TRANSFER, 0)
+            if ret < 1:
+                raise Exception(f"Error stopping transfer profiles: {ret}")
             time.sleep(0.1)
             gen_fun.close()
             break
-        
+
         next(gen_fun)
 
-        data = np.array(
-                [device._buffer_arrays[name] for name in device._channel_names]
-            ).T.flatten()
-        
-        device.data_buffer.append(data)
-        # print(i, device._buffer_arrays["Z"][100])
-        device.update_event.set()
-
-        i += 1
+        buffer.add_to_buffer()
 
